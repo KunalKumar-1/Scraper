d3cb55348987803956e94a4130928df093a9f9d0
KunalKumar-1 <kunaldevspro@gmail.com>
2024-07-15 06:05:31 +0530

    fix: linters & intent

--- getImages.js
+++ getImages.js
@@ -1,69 +1,75 @@
-'use strict'
-    import fs from 'fs';
-    import url from 'url';
-    import path from 'path';
-    import request from 'request';
-    import cheerio from 'cheerio';
-    import { v4 as uuidv4 } from 'uuid';
-    import cliSpinners from 'cli-spinners';
-    import logUpdate from 'log-update';
-   
-    spinner(0);
+"use strict";
+import fs from "fs";
+import url from "url";
+import path from "path";
+import request from "request";
+import cheerio from "cheerio";
+import { v4 as uuidv4 } from "uuid";
+import cliSpinners from "cli-spinners";
+import logUpdate from "log-update";
 
-function spinner(n){
-   let frames = cliSpinners.bouncingBall.frames;
-   let i = 0;
-   if(n===1){
+spinner(0);
+
+function spinner(n) {
+  let frames = cliSpinners.bouncingBall.frames;
+  let i = 0;
+  if (n === 1) {
     process.exit(1);
-   }
-   setInterval(()=>{
-   const frame = frames[i++ % frames.length];
-   logUpdate(frame);
-   },80)};
+  }
+  setInterval(() => {
+    const frame = frames[i++ % frames.length];
+    logUpdate(frame);
+  }, 80);
+}
 
-    function getImages(uri) {
-      request(uri, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(body);
-        const imgs = $("img").toArray();
-        const hostName = url.parse(uri).hostname;
-        const scriptDir = path.dirname(new URL(import.meta.url).pathname);// __dirname; // current script dir
-        const saveDir = path.join(scriptDir, 'images'); 
-        let donwloadCount = 0;
-        
-        if(!fs.existsSync(saveDir)) {
-            fs.mkdirSync(saveDir);
-            }
-        console.log("\n");
-        //console.log(`Download all the images from ${uri}\n\n`);
-        //setTimeout(() => {},10000);
+function getImages(uri) {
+  request(uri, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+      const imgs = $("img").toArray();
+      const hostName = url.parse(uri).hostname;
+      const scriptDir = path.dirname(new URL(import.meta.url).pathname); // __dirname; // current script dir
+      const saveDir = path.join(scriptDir, "images");
+      let donwloadCount = 0;
 
-        imgs.forEach(function (img) {
-         const img_url = img.attribs.src;
+      if (!fs.existsSync(saveDir)) {
+        fs.mkdirSync(saveDir);
+      }
+      console.log("\n");
+      //console.log(`Download all the images from ${uri}\n\n`);
+      //setTimeout(() => {},10000);
 
-            if (/^https?:\/\//.test(img_url)) {
-            // checks if it is https or not
-            const img_name = `${uuidv4()}.jpg`; // like images1.jpeg images2.jpeg ...
-            const img_path = path.join(saveDir, img_name);
+      imgs.forEach(function (img) {
+        const img_url = img.attribs.src;
 
-            request(img_url)
-                .pipe(fs.createWriteStream(img_path))
-                .on('close', function () {
-                donwloadCount++;
-                logUpdate(`\rDownloaded ${donwloadCount}/${imgs.length} => ${img_path}`);
+        if (/^https?:\/\//.test(img_url)) {
+          // checks if it is https or not
+          const img_name = `${uuidv4()}.jpg`; // like images1.jpeg images2.jpeg ...
+          const img_path = path.join(saveDir, img_name);
 
-                if (donwloadCount === imgs.length || donwloadCount === imgs.length-1) {
-                   // clearInterval(intervalId);
-                    console.log("\nDownload completed");
-                    spinner(1);
-                }
-                });
-            //console.log(`Saved: ${img_path}`);
-            }
-         });
-        //console.log("Download completed.")
+          request(img_url)
+            .pipe(fs.createWriteStream(img_path))
+            .on("close", function () {
+              donwloadCount++;
+              logUpdate(
+                `\rDownloaded ${donwloadCount}/${imgs.length} => ${img_path}`
+              );
+
+              if (
+                donwloadCount === imgs.length ||
+                donwloadCount === imgs.length - 1
+              ) {
+                // clearInterval(intervalId);
+                console.log("\nDownload completed");
+                spinner(1);
+              }
+            });
+          //console.log(`Saved: ${img_path}`);
         }
-    });
+      });
+      //console.log("Download completed.")
+    }
+  });
 }
 //Read command-line argument
 const args = process.argv.slice(2); // 'process.argv' extracts the argument including node and scripts
@@ -75,4 +81,3 @@ if (args.length !== 1) {
 //calling getImages() function with the url provided by the user
 const userUrl = args[0];
 getImages(userUrl);
-
